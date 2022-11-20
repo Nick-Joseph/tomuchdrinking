c3977516c68cc5dfd7571a99bdb78628806172fb
Nick-Joseph <njoseph321@gmail.com>
2022-11-20 01:11:29 -0500

    Getting images to work with the counter

--- lib/main.dart
+++ lib/main.dart
@@ -11,17 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.red,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -31,16 +21,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -49,67 +29,89 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  String warningStatus = 'None down';
+  List imgList = [
+    ('assets/images/drinking1.webp'),
+    ('assets/images/drinking2.webp'),
+    ('assets/images/drinking4.jpg'),
+  ];
+  String imgNumber = '';
+  // getImages() {
+  //   for (var i = 0; i < imgList.length; i++) {
+  //     String imgs = imgList[i];
+  //   }
+  // }
+
+  static const String aCoupleDown = 'Pace yourself we chillin';
+  static const String afeweDown =
+      'Feeling really good but\n should probaly stop';
+  static const String tooMucheDown = 'Lets stop and find a way home';
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
+    });
+    if (_counter == 1) {
+      warningStatus = aCoupleDown;
+      imgList[0] = imgNumber;
+    }
+    if (_counter == 4) {
+      warningStatus = afeweDown;
+      imgList[1] = imgNumber;
+    }
+    if (_counter == 6) {
+      warningStatus = tooMucheDown;
+      imgList[2] = imgNumber;
+    } else {}
+  }
+
+  void _clearCounter() {
+    setState(() {
+      _counter = 0;
+      warningStatus = 'None down';
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(warningStatus),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            SizedBox(
+              height: 200,
+              width: 200,
+              child: Image.asset(imgNumber),
+            ),
             const Text(
-              'You have pushed the button this many times:',
+              'How many drinks have you had?',
             ),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
+            ElevatedButton(
+              onPressed: _incrementCounter,
+              child: const Text('Add another drink'),
+            ),
+            ElevatedButton(
+              onPressed: _clearCounter,
+              child: Icon(Icons.refresh),
+            ),
+            const Divider(
+              indent: 50,
+              endIndent: 50,
+              thickness: 2,
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
